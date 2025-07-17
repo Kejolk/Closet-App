63b3b88ccac2021fa7d9dbbe4db624be7e29683d
Arvind_r04 <rarvind248@gmail.com>
2025-07-16 17:01:21 -0700

    added table

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Closet-App
+
+A personal clothing app to help suggest outfits based on what you own, weather, and season.
+
+### MVP Features
+- Add clothes manually (type, color, season)
+- Generate outfit ideas from inputs
+- Future: Recommend based on weather & usage history
+
+### Built with:
+- Streamlit (Python)
--- closet-app/app.py
+++ closet-app/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 
 st.title("Closet App")
@@ -10,5 +11,11 @@ category = st.selectbox("Category", ["Top", "Bottom", "Shoes", "Outerwear"])
 color = st.color_picker("Pick a color")
 season = st.multiselect("Season", ["Spring", "Summer", "Fall", "Winter"])
 
+df = pd.DataFrame(columns= ["Item", "Category", "Color", "Season"])
+
 if st.button("Add item"):
     st.success(f"Added: {item} ({category}, {color}, {', '.join(season)})")
+    new_row = {'Item': [item], 'Category': [category], 'Color': [color], 'Season': [season], }
+    df.loc[len(df)] = new_row
+
+st.dataframe(df)
